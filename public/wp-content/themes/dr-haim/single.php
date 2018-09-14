0eee314f3a55186ad71943110482f460b7c1471c
Victor Nery <victornery.t@gmail.com>
2018-09-14 09:11:31 -0300

    Alteraçao dr haim

--- public/wp-content/themes/dr-haim/single.php
+++ public/wp-content/themes/dr-haim/single.php
@@ -14,6 +14,38 @@
     <div class="haim-content">
         <div class="container">
             <?php the_content(); ?>
+            <?php if(is_singular('cirurgias')): ?>
+                <h2 class="haim-content__title">Outras cirurgias</h2>
+            <?php else: ?>
+                <h2 class="haim-content__title">Outros procedimentos</h2>
+            <?php endif; ?>
+            <ul class="haim-procedures__list haim-procedures__list--internal">
+            <?php if(is_singular('cirurgias')): ?>
+                <?php $cirurgias = new WP_Query(array('post_type' => 'cirurgias', 'posts_per_page' => 4, 'orderby' => 'rand')); ?>
+                <?php while($cirurgias->have_posts()) : $cirurgias->the_post(); ?>
+                    <li class="haim-procedures__item">
+                        <a href="<?php the_permalink(); ?>">
+                            <div class="haim-procedures__mask"></div>
+                            <?php the_post_thumbnail('full'); ?>
+                            <span><?php the_title(); ?></span>
+                        </a>
+                    </li>
+                <?php $i++; endwhile; wp_reset_query(); ?>
+            <?php endif;?>
+
+            <?php if(is_singular('procedimentos')): ?>
+                <?php $procedures = new WP_Query(array('post_type' => 'procedimentos', 'posts_per_page' => 4, 'orderby' => 'rand')); ?>
+                <?php while($procedures->have_posts()) : $procedures->the_post(); ?>
+                    <li class="haim-procedures__item">
+                        <a href="<?php the_permalink(); ?>">
+                            <div class="haim-procedures__mask"></div>
+                            <?php the_post_thumbnail('full'); ?>
+                            <span><?php the_title(); ?></span>
+                        </a>
+                    </li>
+                <?php $i++; endwhile; wp_reset_query(); ?>
+            <?php endif;?>
+            </ul>
         </div>
     </div>
     <?php endwhile; endif; wp_reset_query(); ?>
--- public/wp-content/themes/dr-haim/templates/global/html-newsletter.php
+++ public/wp-content/themes/dr-haim/templates/global/html-newsletter.php
@@ -1,3 +1,14 @@
+<?php if(is_home()): ?>
+    <section class="haim-cta">
+        <div class="container">
+            <?php $setting = get_option('configuracoes_tema'); $settings = $setting['group_ends']; ?>
+            <h2>Marque uma consulta</h2>
+            <?php foreach($settings['end_telefone'] as $tel): ?>
+				<span><a href="tel:<?php echo $tel ?>"><?php echo $tel; ?></a></span>
+			<?php endforeach; ?>
+        </div>
+    </section>
+<?php endif; ?>
 <section class="haim-newsletter">
     <div class="container">
         <span class="haim-newsletter__cta"><b>Fique por dentro</b> | Cadastre-se e receba nossa newsletter</span>
